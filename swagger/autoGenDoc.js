916de9a74b37d82791ae11968b947d8d91f142e0
RafaelLoureiro <rafanetmello@gmail.com>
2023-08-07 21:09:43 -0300

    add new files for back routs-swagger

---
+++ swagger/autoGenDoc.js
@@ -0,0 +1,40 @@
+const mongooseToSwagger = require('mongoose-to-swagger');
+//const EsquemaTarefa =  require ('../src/modules/tarefa.js');
+//const EsquemaUsuario =  require ('../src/modules/usuario.js');
+const swaggerAutogen = require('swagger-autogen')({
+    openapi: '3.0.0',
+    language: 'pt-BR',
+});
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['../index.js', /*'../src/routes.js'*/];
+
+let doc = {
+    info: {
+        version: '1.0.0',
+        title: "API do BoardTasks",
+        description: "Documentação da API do BoardTasks."
+    },
+    servers: [
+        {
+            url: "http://localhost:4000",
+            description: "Servidor localhost"
+        },
+    ],
+    consumes: ['application/json'],
+    produces: ['application/json'],
+    /*
+    components: {
+        schemas:{
+            Usuario: mongooseToSwagger(EsquemaUsuario),
+            Tarefa: mongooseToSwagger(EsquemaTarefa),
+        }
+    }*/
+}
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    console.log("Documentação do Swagger gerada encontra-se no arquivo em: " + outputFile);
+    if (process.env.NODE_ENV !== 'production') {
+        require("../index.js");
+    }
+})
